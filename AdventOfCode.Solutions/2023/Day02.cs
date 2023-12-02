5a2cecb40bb58abf338b884575c95e2dff60c9ac
Krzysiekm <kicmorikic@gmail.com>
2023-12-02 10:26:11 +0100

    Day02 solved

--- AdventOfCode.Solutions/2023/Day02.cs
+++ AdventOfCode.Solutions/2023/Day02.cs
@@ -2,18 +2,18 @@
 
 public class Day02 : IChallangeSolution
 {
-    private Dictionary<string, int> BagContents = new Dictionary<string, int>()
+    private Dictionary<string, int> _bagContents = new Dictionary<string, int>()
     {
         {"red", 12},
         {"green", 13},
         {"blue", 14}
     };
     public string ChallangeName => "2023, day 2: Cube Conundrum";
-    public bool HasSecondStage => false;
+    public bool HasSecondStage => true;
     
     public string FirstStageInputPath => "Data/2023/02/01.txt";
 
-    public string SecondStageInputPath => "Data/2023/02/02.txt";
+    public string SecondStageInputPath => "Data/2023/02/01.txt";
 
     public string GetFirstStageResult(IEnumerable<string> input)
     {
@@ -31,12 +31,78 @@ public class Day02 : IChallangeSolution
 
     public string GetSecondStageResult(IEnumerable<string> input)
     {
-        throw new NotImplementedException();
+        var sumOfProducts = 0;
+        foreach (var gameResult in input)
+        {
+            Dictionary<string,int> minimumSet = getMinimumSetFromTheGameResult(gameResult);
+            int minimumCountsMultiplied = 1;
+            foreach (var i in minimumSet)
+            {
+                minimumCountsMultiplied *= i.Value;
+            }
+
+            sumOfProducts += minimumCountsMultiplied;
+        }
+        return sumOfProducts.ToString();
+    }
+
+    private Dictionary<string, int> getMinimumSetFromTheGameResult(string gameResult)
+    {
+        Dictionary<string, int>  resultDictionary = _bagContents.ToDictionary(pair => pair.Key, pair => 0);
+        foreach (var gameStage in GetGameRecordDictionary(gameResult))
+        {
+            if(resultDictionary[gameStage.Key] < gameStage.Value)
+                resultDictionary[gameStage.Key] = gameStage.Value;
+        }
+        return resultDictionary;
     }
 
     private bool IsGamePossible(string gameResult, out int i)
     {
         i = 0;
-        return false;
+        int gameId = GetGameID(gameResult);
+        List<KeyValuePair<string,int>> gameRecord = GetGameRecordDictionary(gameResult);
+        foreach (var gameStageKVP in gameRecord)
+        {
+            if (_bagContents[gameStageKVP.Key] < gameStageKVP.Value)
+            {
+                return false;
+            }
+        }
+
+        i = gameId;
+        return true;
+    }
+
+    private List<KeyValuePair<string,int>> GetGameRecordDictionary(string gameResult)
+    {
+        List<KeyValuePair<string,int>> gameRecordDictionary = new();
+        var gameResultString= gameResult.Split(':')[1];
+        var gameStages = gameResultString.Split(new []{',',';'});
+        foreach (var stageString in gameStages)
+        {
+            var stageSplit = stageString.Trim().Split(" ");
+            try
+            {
+                int count = int.Parse(stageSplit[0]);
+                string name = stageSplit[1];
+                gameRecordDictionary.Add(new KeyValuePair<string, int>(name,count));
+            }
+            catch(Exception e) 
+            {
+                throw new ArgumentException($"String '{gameResult}' is in a wrong format");
+            }
+        }
+        return gameRecordDictionary;
+    }
+
+    private int GetGameID(string gameResult)
+    {
+        var gameIdentifier= gameResult.Split(':')[0];
+        if (int.TryParse(gameIdentifier.Split(' ')[1], out int result))
+        {
+            return result;
+        }
+        throw new ArgumentException($"String: {gameResult} is in a wrong format");
     }
 }
--- AdventOfCode.Tests/Day02Tests.cs
+++ AdventOfCode.Tests/Day02Tests.cs
@@ -18,13 +18,11 @@ namespace _2023
         };
         public static IEnumerable<object[]> _sampleDataStage2 = new List<object[]>
         {
-            new []{"two1nine", "29"},
-            new []{"eightwothree","83"},
-            new []{"abcone2threexyz","13"},
-            new []{"xtwone3four","24" },
-            new []{"4nineeightseven2","42" },
-            new []{"zoneight234","14" },
-            new []{"7pqrstsixteen","76" }
+            new []{"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green", "48"},
+            new []{"Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue","12"},
+            new []{"Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red","1560"},
+            new []{"Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red","630" },
+            new []{"Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green","36" }
         };
 
 
@@ -67,7 +65,7 @@ namespace _2023
                 input.Add((string)objects[0]);
             }
             var result = _sut.GetSecondStageResult(input);
-            result.Should().Be("281");
+            result.Should().Be("2286");
         }
     }
 }
